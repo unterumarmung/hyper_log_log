a0ec297f7b051ae83b872975f3b6985702d9a31a
unterumarmung <umarmungenfuerimmer@gmail.com>
2019-08-24 21:43:01 +0400

    Added details file

---
+++ hll/details.hxx
@@ -0,0 +1,22 @@
+#ifndef HLL_DETAILS_HXX
+#define HLL_DETAILS_HXX
+
+#include <type_traits>
+
+namespace hll { namespace details {
+
+#if __cplusplus >= 201402L
+
+#define HLL_CONSTEXPR_OR_INLINE_FUNCTION constexpr
+
+#else
+
+#define HLL_CONSTEXPR_OR_INLINE_FUNCTION inline
+
+#endif // __cplusplus >= 201402L
+
+
+} // namespace details
+} // namespace hll
+
+#endif // HLL_DETAILS_HXX
