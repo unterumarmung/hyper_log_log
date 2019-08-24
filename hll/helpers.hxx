317e7d67bb7fd37a1b3634f77d00b91d6e932c3b
unterumarmung <umarmungenfuerimmer@gmail.com>
2019-08-24 23:28:01 +0400

    Added doxygen comment

--- hll/helpers.hxx
+++ hll/helpers.hxx
@@ -1,7 +1,13 @@
+/**
+ * @file hll/heplers.hxx
+ * @brief A constexpr C++11 implementations of some STL functions
+ * @author Daniil Dudkin (unterumarmung)
+ */
 #ifndef HYPER_LOG_LOG_HELPERS_HXX
 #define HYPER_LOG_LOG_HELPERS_HXX
 
 namespace hll { namespace helpers {
+
 template <typename T>
 constexpr const T& max(const T& lhs, const T& rhs) noexcept(noexcept(lhs > rhs))
 {
