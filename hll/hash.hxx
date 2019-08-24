7bbe825a274b70887892fdb6a8fd6aff697d468f
unterumarmung <umarmungenfuerimmer@gmail.com>
2019-08-24 21:44:40 +0400

    Decomposed a type trait

--- hll/hash.hxx
+++ hll/hash.hxx
@@ -31,16 +31,14 @@ namespace hll
 
     /**
      * Hashes "random-access" containers of the fundamental types
-     * @tparam T the container type, must have T::size and T::data member functions and  T::value_type member type
+     * @tparam T the container type, must have T::size and T::data member functions and T::value_type member type
      * @param value the container
      * @return hash
      */
     template <typename T>
     hash_result hash(const T& value,
             typename std::enable_if<
-               hll::traits::has_data_member_function<T>::value    // has T::data
-            && hll::traits::has_size_member_function<T>::value    // has T::size
-            && std::is_fundamental<typename T::value_type>::value // value_type is fundamental
+                    hll::traits::is_ra_fundamental_container <T>::value
             >::type* = nullptr)
     {
         auto hash_result = murmur_hash(value.data(), value.size() * sizeof(T::value_type), /*seed = */ 0);
