1293ff4a7136e31fae60cdbe460837af465bf46e
unterumarmung <umarmungenfuerimmer@gmail.com>
2019-08-25 22:58:52 +0400

    Added constexpr array_fill function

--- hll/helpers.hxx
+++ hll/helpers.hxx
@@ -6,12 +6,26 @@
 #ifndef HYPER_LOG_LOG_HELPERS_HXX
 #define HYPER_LOG_LOG_HELPERS_HXX
 
+#include <array>
+#include <type_traits> // std::is_nothrow_copy_assignable
+#include "details.hxx" // HLL_CONSTEXPR_OR_INLINE
+
 namespace hll { namespace helpers {
 
 template <typename T>
 constexpr const T& max(const T& lhs, const T& rhs) noexcept(noexcept(lhs > rhs))
 {
     return lhs > rhs ? lhs : rhs;
+}
+
+template <typename T, std::size_t N>
+HLL_CONSTEXPR_OR_INLINE void array_fill(std::array<T, N>& array, const T& filler)
+                                                    noexcept(std::is_nothrow_copy_assignable<T>::value)
+{
+    for (std::size_t i = 0; i < N; ++i)
+    {
+        array[i] = filler;
+    }
 }
 
 } // namespace helpers
