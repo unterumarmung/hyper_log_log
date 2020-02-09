5964e72e860a3aea219259bd826dd9c6c9f082dd
unterumarmung <umarmungenfuerimmer@gmail.com>
2020-02-10 01:19:55 +0400

    Changed operator='s return type

--- hll/hyper_log_log.hxx
+++ hll/hyper_log_log.hxx
@@ -62,9 +62,9 @@ public:
         static_assert(k >= 4 && k <= 30, "k must be in a range [4; 30]");
     }
     constexpr hyper_log_log(const this_type& rhs) = default;
-    constexpr this_type& operator=(const this_type& rhs) = default;
+    constexpr hyper_log_log& operator=(const this_type& rhs) = default;
     constexpr hyper_log_log(this_type&&) noexcept = default;
-    constexpr this_type& operator=(this_type&&) noexcept = default;
+    constexpr hyper_log_log& operator=(this_type&&) noexcept = default;
 
     ~hyper_log_log() = default;
 
