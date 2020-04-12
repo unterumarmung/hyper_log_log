d324f7dcb9f8740a13e0bf017cdf678c54373055
unterumarmung <umarmungenfuerimmer@gmail.com>
2020-04-13 02:14:39 +0400

    Style changes

--- hll/murmur_hash.hxx
+++ hll/murmur_hash.hxx
@@ -17,7 +17,7 @@
  * @param seed
  * @return hash
  */
-HLL_CONSTEXPR_OR_INLINE uint32_t murmur_hash(const void *key, uint32_t length, uint32_t seed) noexcept
+HLL_CONSTEXPR_OR_INLINE uint32_t murmur_hash(const void* key, uint32_t length, uint32_t seed) noexcept
 {
     constexpr uint32_t c1 = 0xcc9e2d51;
     constexpr uint32_t c2 = 0x1b873593;
@@ -26,8 +26,8 @@ HLL_CONSTEXPR_OR_INLINE uint32_t murmur_hash(const void *key, uint32_t length, u
     constexpr uint32_t m = 5;
     constexpr uint32_t n = 0xe6546b64;
     const auto chunk_length = length / 4u;
-    const auto chunks = static_cast<const uint32_t *>(key); // 32 bit extract from `key'
-    const auto tail = static_cast<const uint8_t *>(key) + chunk_length * 4; // tail - last 8 bytes
+    const auto chunks = static_cast<const uint32_t*>(key); // 32 bit extract from `key'
+    const auto tail = static_cast<const uint8_t*>(key) + chunk_length * 4; // tail - last 8 bytes
     uint32_t h = 0;
     uint32_t k = 0;
 
@@ -55,8 +55,10 @@ HLL_CONSTEXPR_OR_INLINE uint32_t murmur_hash(const void *key, uint32_t length, u
     // remainder
     switch (length & 3u)
     { // `length % 4'
-        case 3: k ^= (tail[2] << 16u);
-        case 2: k ^= (tail[1] << 8u);
+        case 3:
+            k ^= (tail[2] << 16u);
+        case 2:
+            k ^= (tail[1] << 8u);
 
         case 1:
             k ^= tail[0];
