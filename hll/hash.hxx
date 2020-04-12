bad222b7771a8d9d035ac338d6a8705c5ec5aed7
unterumarmung <umarmungenfuerimmer@gmail.com>
2020-04-13 02:15:05 +0400

    Style changes

--- hll/hash.hxx
+++ hll/hash.hxx
@@ -13,33 +13,33 @@
 
 namespace hll
 {
-    /// type alias for hash functions return-type
-    using hash_result = uint32_t;
+/// type alias for hash functions return-type
+using hash_result = uint32_t;
 
-    /**
-     * Hashes the fundamental types
-     * @tparam T the value type
-     * @param value the value
-     * @return hash
-     */
-    template <typename T, typename std::enable_if<std::is_fundamental<T>::value>::type* = nullptr>
-    constexpr hash_result hash(const T& value) noexcept
-    {
-        return murmur_hash(&value, sizeof(T), /*seed = */ 0);
-    }
+/**
+ * Hashes the fundamental types
+ * @tparam T the value type
+ * @param value the value
+ * @return hash
+ */
+template<typename T, typename std::enable_if<std::is_fundamental<T>::value>::type* = nullptr>
+constexpr hash_result hash(const T& value) noexcept
+{
+    return murmur_hash(&value, sizeof(T), /*seed = */ 0);
+}
 
-    /**
-     * Hashes "random-access" containers of the fundamental types
-     * @tparam T the container type, must have T::size and T::data member functions and T::value_type member type
-     * @param value the container
-     * @return hash
-     */
-    template <typename T, typename std::enable_if<hll::traits::is_ra_fundamental_container <T>::value>::type* = nullptr>
-    constexpr hash_result hash(const T& value)
-            noexcept(noexcept(value.data()) && noexcept(value.size()))
-    {
-        return murmur_hash(value.data(), value.size() * sizeof(T::value_type), /*seed = */ 0);
-    }
+/**
+ * Hashes "random-access" containers of the fundamental types
+ * @tparam T the container type, must have T::size and T::data member functions and T::value_type member type
+ * @param value the container
+ * @return hash
+ */
+template<typename T, typename std::enable_if<hll::traits::is_ra_fundamental_container<T>::value>::type* = nullptr>
+constexpr hash_result hash(const T& value)
+noexcept(noexcept(value.data()) && noexcept(value.size()))
+{
+    return murmur_hash(value.data(), value.size() * sizeof(T::value_type), /*seed = */ 0);
+}
 
 } //namespace hll
 
